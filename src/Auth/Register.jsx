6e0042ea9d8c018449b20abfe1b0f6cca2e9741f
Ivan Trujillo <ivanrts43@gmail.com>
2020-09-16 23:01:29 -0400

    Register realignment

--- src/Auth/Register.jsx
+++ src/Auth/Register.jsx
@@ -42,46 +42,31 @@ export default function Register(){
     }
     return(
         <div className="container-fluid">
-  <div className="row">
-    <div className="col">
-    <h1 className="display-4"> Register a new account</h1>
-    </div>
-  </div>
-      &nbsp;
-  <div className="row">
-    <div className="col">
-    </div>
-    <div className="col">
-    <form>
-            <div className="form-group">
-                <label htmlFor="registerEmailInput">Email address</label>
-                <input onChange={(e) => handleEmail(e)} type="email" className="form-control" id="registerEmail" aria-describedby="registerEmailHelp" placeholder="Enter email" />
-                <small id="registerEmailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+            <div className="row justify-content-md-center">
+                <div className="col col-lg-6">
+                    <h1 className="display-4"> Register a new account</h1>
+                    <form>
+                        <div className="form-group">
+                            <label htmlFor="registerEmailInput">Email address</label>
+                            <input onChange={(e) => handleEmail(e)} type="email" className="form-control" id="registerEmail" aria-describedby="registerEmailHelp" placeholder="Enter email" />
+                            <small id="registerEmailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="registerPassword">Password</label>
+                            <input onChange={(e) => handlePassword(e)} type="password" className="form-control" id="registerPassword" placeholder="Password"/>
+                        </div>
+                        <div className="row">
+                            <div className="col">
+                                    <button onClick={() => returnToLogin()} className="btn btn-primary">Back to Login</button>          
+                            </div>
+                            <div className="col">
+                                    <button type="submit" className="btn btn-primary" onClick={(e) => registerUser(e)}>Submit</button>   
+                            </div>
+                        </div>
+                    </form>
             </div>
-            <div className="form-group">
-                <label htmlFor="registerPassword">Password</label>
-                <input onChange={(e) => handlePassword(e)} type="password" className="form-control" id="registerPassword" placeholder="Password"/>
-            </div>
-            <div className="row">
-                <div className="col">
-                        <button onClick={() => returnToLogin()} className="btn btn-primary">Back to Login</button>          
-                </div>
-                <div className="col">
-                        <button type="submit" className="btn btn-primary" onClick={(e) => registerUser(e)}>Submit</button>   
-                </div>
-            </div>
-        </form>
+        </div>
     </div>
-    <div className="col">
-    </div>
-    <div className="row">
-    <div className="col">
-    </div>
-    <div className="col">
-    </div>
-  </div>
-  </div>
-</div>
 
     )
 }
